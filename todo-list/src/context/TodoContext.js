9aa5ebbe5d264ea6cc329b75c30817743c854b2c
Manh Nguyen Sy <manhns@Manhs-MacBook-Pro.local>
2024-03-11 17:30:42 +0700

    update

---
+++ todo-list/src/context/TodoContext.js
@@ -0,0 +1,30 @@
+import React, { createContext, useState } from "react";
+
+export const TodoContext = createContext();
+
+export const TodoProvider = ({ children }) => {
+  const [todos, setTodos] = useState([]);
+
+  const idTodo = todos.reduce(
+    (max, todo) => (todo.id > max ? todo.id : max),
+    0
+  );
+
+  const addTodo = (todo) => {
+    setTodos([...todos, { id: idTodo + 1, ...todo }]);
+  };
+
+  const updateTodo = (id, updatedTodo) => {
+    setTodos(todos.map((todo) => (todo.id === id ? updatedTodo : todo)));
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <TodoContext.Provider value={{ todos, addTodo, updateTodo, deleteTodo }}>
+      {children}
+    </TodoContext.Provider>
+  );
+};
