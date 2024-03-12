77ff2717c9aa7266ccbdf74666bc40442235a82b
Manh Nguyen Sy <manhns@Manhs-MacBook-Pro.local>
2024-03-12 09:05:10 +0700

    update

--- demo-hooks/src/use-reducer/UseReducer.js
+++ demo-hooks/src/use-reducer/UseReducer.js
@@ -16,7 +16,9 @@ const reducer = (state, action) => {
 const UseReducer1 = () => {
   console.log("UseReducer1 rendered");
   const [state, dispatch] = useReducer(reducer, initialState);
+  console.log("reducer", reducer);
   console.log("state", state);
+  console.log("initialState", initialState);
 
   return (
     <div>
